05d0b5de62900ab50282b293e6b4cd3f06201631
Ali Karimi <ak2665622@gmail.com>
2022-09-19 13:54:43 +0430

    isolate part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,40 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/extensions.dart';
 import 'package:flutter_application_1/mixin_class.dart';
+import 'package:flutter_application_1/person.dart';
 
 Future<void> testIt() async {
   final people = await GetPeople().getString();
   people.log();
+}
+
+Future<Iterable<Person>> getPersons() async {
+  final rp = ReceivePort();
+  await Isolate.spawn(_getPersons, rp.sendPort);
+  return await rp.first;
+}
+
+void _getPersons(SendPort port) async {
+  const url = 'http://127.0.0.1:5500/apis/people1.json';
+  final persons = await HttpClient()
+      .getUrl(Uri.parse(url))
+      .then((req) => req.close())
+      .then((res) => res.transform(utf8.decoder).join())
+      .then((jsonString) => json.decode(jsonString) as List<dynamic>)
+      .then(
+        (json) => json.map(
+          (map) => Person.fromJson(map),
+        ),
+      );
+  // port.send(persons);
+  Isolate.exit(
+    port,
+    persons,
+  );
 }
 
 void main() {
@@ -47,7 +77,14 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            TextButton(
+                onPressed: () async {
+                  final persons = await getPersons();
+                  persons.log();
+                },
+                child: const Text('Press me'))
+          ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
